d56a5cd04633a946ba2b41a685e22afb12dd9f81
Lamberto Basti <lamberto.basti@jetbrains.com>
2021-04-17 17:54:25 +0200

    Initial commit!

--- puppeteer-core-externals/src/main/kotlin/puppeteer/Externals.kt
+++ puppeteer-core-externals/src/main/kotlin/puppeteer/Externals.kt
@@ -3,7 +3,21 @@
 
 package puppeteer
 
-external fun launch(options: LaunchOptions): Browser
+
+import child_process.ChildProcess
+import kotlin.js.Promise
+
+external fun launch(options: LaunchOptions): Promise<Browser>
+
+/**
+ * This methods attaches Puppeteer to an existing browser instance.
+ */
+external fun connect(options: ConnectOptions): Promise<Browser>
+
+/**
+ * Clears all registered handlers.
+ */
+external fun clearCustomQueryHandlers(): Unit
 
 external val networkConditions: Map<String, PredefinedNetworkConditions>
 
@@ -17,9 +31,46 @@ external interface CustomQueryHandler {
 }
 
 external interface PredefinedNetworkConditions {
-    val download: Double
-    val upload: Double
-    val latency: Int
+    var download: Double
+    var upload: Double
+    var latency: Int
+}
+
+external interface ConnectOptions {
+    /**
+     * a browser websocket endpoint to connect t
+     */
+    var browserWSEndpoint: String?
+
+    /**
+     * a browser url to connect to, in format `http://${host}:${port}`. Use interchangeably with browserWSEndpoint to let Puppeteer fetch it from [metadata endpoint](https://chromedevtools.github.io/devtools-protocol/#how-do-i-access-the-browser-target).
+     */
+    var browserURL: String?
+
+    /**
+     * Whether to ignore HTTPS errors during navigation. Defaults to false.
+     */
+    var ignoreHTTPSErrors: Boolean
+
+    /**
+     * Sets a consistent viewport for each page. Defaults to an 800x600 viewport. null disables the default viewport.
+     */
+    var defaultViewport: ViewPort?
+
+    /**
+     * Slows down Puppeteer operations by the specified amount of milliseconds. Useful so that you can see what is going on.
+     */
+    var slowMo: Boolean
+
+    /**
+     * Experimental Specify a custom transport object for Puppeteer to use.
+     */
+    var transport: dynamic
+
+    /**
+     * Possible values are: chrome, firefox. Defaults to chrome.
+     */
+    var product: String
 }
 
 external interface LaunchOptions {
@@ -52,6 +103,38 @@ external interface ViewPort {
     var isLandscape: Boolean
 }
 
-external interface Browser {
+external interface Browser : EventEmitter {
+    fun browserContexts(): Array<BrowserContext>
+    fun close(): Promise<Unit>
+    fun createIncognitoBrowserContext(): Promise<BrowserContext>
+    fun defaultBrowserContext(): BrowserContext
+    fun disconnect(): Boolean
+    fun isConnected(): Boolean
+    fun newPage(): Promise<Page>
+    fun pages(): Promise<Array<Page>>
+    fun process(): ChildProcess?
+    fun target(): Target
+    fun targets(): Array<Target>
+    fun userAgent(): Promise<String>
+    fun version(): Promise<String>
+    fun waitForTarget()
+}
 
+external interface Target {
+
+}
+
+external interface Page {
+
+}
+
+external interface BrowserContext : EventEmitter {
+
+}
+
+external interface EventEmitter {
+    fun addListener(event: String, handler: (dynamic) -> Unit): EventEmitter
+    fun emit(event: String, eventData: dynamic)
+
+    // incomplete
 }
